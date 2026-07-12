df54e264057c624ba4565cfd6cd5ec292ccc6f89
Greg <gregory.sabella@impact-initiatives.org>
2026-07-12 14:15:30 +0300

    remove use of .env

--- api/src/api/config.py
+++ api/src/api/config.py
@@ -3,11 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config: SettingsConfigDict = SettingsConfigDict(
-        env_file=".env",
-        env_ignore_empty=True,
-        extra="ignore",
-    )
+    # model_config: SettingsConfigDict = SettingsConfigDict(
+    #     env_file=".env",
+    #     env_ignore_empty=True,
+    #     extra="ignore",
+    # )
 
     queue_connection_string: str = Field(alias="AZURE_STORAGE_CONNECTION_STRING")
     queue_name: str = Field(
